71e83963319f1d992015adc9c017706053781476
Azazel <Profession123122@gmail.com>
2025-08-03 21:58:58 +0200

    Add files via upload

---
+++ Task1/1task.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+import socket
+from dotenv import load_dotenv
+
+load_dotenv()
+author_name = os.environ.get("AUTHOR")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    hostname = socket.gethostname()
+    server_ip = socket.gethostbyname(hostname)
+    return render_template('index.html',
+        hostname=hostname,
+        ip_address=server_ip,
+        author=author_name)
+
+
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8000,debug=True)
